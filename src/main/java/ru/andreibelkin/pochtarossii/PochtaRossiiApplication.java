22d94b554460298a0ddaf4a5f06702563d79dca2
Белкин, Андрей <andrei.belkin@pm.me>
2021-08-25 22:33:05 -0400

    ПР6-3 : Создал новый проект на start.spring.io

---
+++ src/main/java/ru/andreibelkin/pochtarossii/PochtaRossiiApplication.java
@@ -0,0 +1,13 @@
+package ru.andreibelkin.pochtarossii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochtaRossiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PochtaRossiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andreibelkin/pochtarossii/PochtaRossiiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andreibelkin.pochtarossii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochtaRossiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
